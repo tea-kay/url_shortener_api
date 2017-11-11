12a25c72ed0c810fbf313be57c279fbd2a707839
Timothy Kim <tkim1202@gmail.com>
2017-11-11 21:59:10 +0900

    Added URL validator and shortId

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var validUrl = require('valid-url');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -6,9 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/:url', function(req, res, next) {
-  var newUrl = req.params.url;
-  res.render('index', { url: newUrl });
+router.get('(*)/:url', function(req, res, next) {
+  var { url } = req.params;
+  var fullUrl = "https://" + url;
+
+  if (validUrl.isUri(fullUrl)) {
+    res.render('index', { fullUrl });
+  } else {
+    res.json({ error: "Not a valid URL" });
+  }
 });
 
 module.exports = router;
